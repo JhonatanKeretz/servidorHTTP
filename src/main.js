566d19b01200140493cf301c948088f7eb3e489e
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-04-27 23:20:39 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'mutationobserver-shim'
+import '@babel/polyfill'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+
+import './plugins/bootstrap-vue'
+import './plugins/axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
